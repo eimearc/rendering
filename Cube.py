344912be0e08f60c788096b81cbf7bf4fc7bd14a
Eimear Crotty <eimear.crotty@umail.ucc.ie>
2020-04-19 13:19:53 +0100

    Added edge loop and made smoother.

--- Cube.py
+++ Cube.py
@@ -317,14 +317,21 @@ def Cylinder(radius=0.5, height=1.0):
 
 	# 5
 	x=X_BASE
+	y=height*0.8
+	z=Z_BASE
+	top_verts_outer_bottom = Square(x,y,z)
+
+	# 6
+	x=X_BASE
 	y=height*0.9
 	z=Z_BASE
-	top_verts_outer_bottom = Square(x,y,z) # Needs to be smoother
+	top_verts_outer_middle_bottom = Square(x,y,z)
 
+	# 7
 	x=X_BASE*1.05
 	y=height*0.95
 	z=Z_BASE*1.05
-	top_verts_outer_middle = Square(x,y,z)
+	top_verts_outer_middle_top = Square(x,y,z)
 
 	x=X_BASE
 	y=height
@@ -337,7 +344,7 @@ def Cylinder(radius=0.5, height=1.0):
 	top_verts_inner = Square(x,y,z)
 
 	bottom_verts_outer = bottom_verts_inner_top + bottom_verts_inner_bottom + bottom_verts_outer_bottom + bottom_verts_outer_middle + bottom_verts_outer_top
-	top_verts = top_verts_outer_bottom + top_verts_outer_middle + top_verts_outer_top + top_verts_inner
+	top_verts = top_verts_outer_bottom + top_verts_outer_middle_bottom + top_verts_outer_middle_top + top_verts_outer_top + top_verts_inner
 	
 	x=X_BASE*0.9
 	y=LIP_HEIGHT*2
@@ -346,7 +353,7 @@ def Cylinder(radius=0.5, height=1.0):
 
 	verts = bottom_verts_outer + top_verts + bottom_verts_inner
 
-	num = 9
+	num = 10
 	edgeloops = []
 	indices = [
 		0,1,2,3
@@ -370,6 +377,7 @@ def Cylinder(radius=0.5, height=1.0):
 	# Change crease for final edgeloop to be more crisp.
 	floatargs[num-1]=3
 	floatargs[6]=0.1
+	floatargs[7]=0.1
 
 	ri.SubdivisionMesh("catmull-clark", nverts, indices, tags, nargs, edgeloops, floatargs, {ri.P: verts})
 
